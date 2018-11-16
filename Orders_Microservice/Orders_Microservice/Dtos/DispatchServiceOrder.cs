cd2c3e0b55f20f710aafe6a2e920417d622ec7db
WINDOWS\q5081793 <Q5081793@live.tees.ac.uk>
2018-11-16 13:55:33 +0000

    Initial Project Build and started Dtos

---
+++ Orders_Microservice/Orders_Microservice/Dtos/DispatchServiceOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orders_Microservice.Dtos
+{
+    public class DispatchServiceOrder
+    {
+        public int DispatchId { get; set; }
+        public string OrderRef { get; set; }
+        public string ProductRef { get; set; }
+        public int Quantity { get; set; }
+        public string Address { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public DateTime DispatchDate{ get; set; }
+    }
+}
